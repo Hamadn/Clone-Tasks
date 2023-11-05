4206f5defb66fef93d13f6bebcc7608b125336b8
Hamadn <nhamad919@gmail.com>
2023-11-05 20:28:29 +0400

    Added the overlay and menu toggle for mobile

---
+++ Task 2/js/index.js
@@ -0,0 +1,27 @@
+/*Storing the different elements with classes and ids in variables to be used later in code*/
+const hamburgerBtn = document.querySelector("#hamburgerBtn");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+// Function to add the eventlistener to the menu toggle in the header
+hamburgerBtn.addEventListener("click", function () {
+  // If statement to check if the menu toggle is opened
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } // Else statement to check if it is not opened
+  else {
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
